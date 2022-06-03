4304a16fa318df6483661b2dd8e9b602f161b57d
symplybennie <benedictanwa19@gmail.com>
2022-06-03 03:06:10 -0700

    shell headers

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+
+int main(int ac, char **av);
+int shell_cd(char **args);
+int shell_exit(char **args);
+int num_builtins(void);
+int launch(char **args);
+char *read_line(void);
+int execute(char **args);
+void shell_loop(void);
+char **split_line(char *line);
+char *_getenv(char *name);
+char *_which(char *str1, char *str2);
+int _path(char **argv);
+#endif
